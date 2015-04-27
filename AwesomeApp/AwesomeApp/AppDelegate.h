2b2f95e738f84da68e25bb75b154bb893b5eb7fa
Alejandro <aflores@tarjetasdelnoreste.com>
2015-04-27 11:02:35 -0500

    Primer Commit

---
+++ AwesomeApp/AwesomeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AwesomeApp
+//
+//  Created by TNMacBook on 27/04/15.
+//  Copyright (c) 2015 TNMacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
